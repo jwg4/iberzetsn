f77ef930accf612435fe9f0f29b9e0a5cce3bc55
Jack Grahl <jack.grahl@gmail.com>
2017-09-27 00:23:18 +0000

    Spacing fixes.

--- tests/test_iberzetsn.py
+++ tests/test_iberzetsn.py
@@ -7,6 +7,7 @@ class TestYiddishToRoman(unittest.TestCase):
     def test_simple_word(self):
         self.assertEqual(yiddish_to_roman("שול"), "shul")
 
+
 class TestRomanToYiddish(unittest.TestCase):
     def test_simple_word(self):
         self.assertEqual(roman_to_yiddish("yiddish"), "ייִדיש")
