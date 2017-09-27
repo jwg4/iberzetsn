22d18322d565e0729fc44b426653b2ddbd13b845
Jack Grahl <jack.grahl@gmail.com>
2017-09-27 00:10:10 +0000

    Implement roman_to_yiddish in a basic way.

--- iberzetsn/__init__.py
+++ iberzetsn/__init__.py
@@ -1,15 +1,46 @@
-YIDDISH_LETTERS = {
-    "ש": "sh",
-    "ו": "u",
-    "ל": "l",
+YIDDISH_LETTERS = [
+    ("ש", "sh"),
+    ("ו", "u"),
+    ("ל", "l"),
+    ("ד", "d"),
+    ("י", "i"),
+]
+
+YIDDISH_LETTER_LOOKUP = {
+    x[0]: x[1] for x in YIDDISH_LETTERS
 }
+
+ROMAN_LETTER_LOOKUP = {
+    x[1]: x[0] for x in YIDDISH_LETTERS
+}
+
+ROMAN_ALIAS_LOOKUP = {
+    "dd": "d"
+}
+
+ROMAN_COMPOSITE_LOOKUP = {
+    "yi": "ייִ"
+}
+
+ROMAN_LOOKUP = ROMAN_LETTER_LOOKUP.copy()
+ROMAN_LOOKUP.update(ROMAN_COMPOSITE_LOOKUP)
+ROMAN_LOOKUP.update({ x: ROMAN_LETTER_LOOKUP[ROMAN_ALIAS_LOOKUP[x]] for x in ROMAN_ALIAS_LOOKUP })
+
 
 def yiddish_to_roman(s):
     r = []
     for c in s:
-        if c in YIDDISH_LETTERS:
-            r.append(YIDDISH_LETTERS[c])
+        if c in YIDDISH_LETTER_LOOKUP:
+            r.append(YIDDISH_LETTER_LOOKUP[c])
     return "".join(r)
 
+
 def roman_to_yiddish(s):
-    return ""
+    if not s:
+        return ""
+    prefixes = [ x for x in ROMAN_LOOKUP if s.startswith(x) ]
+    if prefixes:
+        prefixes.sort(key=len)
+        x = prefixes[-1]
+        return ROMAN_LOOKUP[x] + roman_to_yiddish(s[len(x):])
+    raise ValueError("Couldn't find the first Yiddish character of %s" % (s, ))
--- tests/test_iberzetsn.py
+++ tests/test_iberzetsn.py
@@ -6,3 +6,7 @@ from iberzetsn import yiddish_to_roman, roman_to_yiddish
 class TestYiddishToRoman(unittest.TestCase):
     def test_simple_word(self):
         self.assertEqual(yiddish_to_roman("שול"), "shul")
+
+class TestRomanToYiddish(unittest.TestCase):
+    def test_simple_word(self):
+        self.assertEqual(roman_to_yiddish("yiddish"), "ייִדיש")
