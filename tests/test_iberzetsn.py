41fbfed6ceaaf3ecea1e2ed2c4bf8918e587a6df
Jack Grahl <jack.grahl@gmail.com>
2017-10-06 23:08:13 +0000

    Add a new test case: "dos broyt".

--- tests/test_iberzetsn.py
+++ tests/test_iberzetsn.py
@@ -14,3 +14,6 @@ class TestYiddishToRoman(unittest.TestCase):
 class TestRomanToYiddish(unittest.TestCase):
     def test_simple_word(self):
         self.assertEqual(roman_to_yiddish("yiddish"), "ייִדיש")
+
+    def test_simple_two_word_phrase(self):
+        self.assertEqual(roman_to_yiddish("dos broyt"), "דאׇס ברױט")
