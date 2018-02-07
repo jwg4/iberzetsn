44a6de629fa8bee3fbfb73ed65ef8e8874de4d01
Ubuntu <jack@garonne.mihjbb4c2viedktwapc15cx44h.zx.internal.cloudapp.net>
2018-02-07 23:26:42 +0000

    Bump version 0.1.1._

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iberzetsn',
-    version='0.1.0',
+    version='0.1.1',
     description='A library for Yiddish transliteration',
     url='https://github.com/jwg4/iberzetsn',
     author='Jack Grahl',
